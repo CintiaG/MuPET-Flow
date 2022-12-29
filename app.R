e52147bb889eb3018e15704803809499a098eebe
Cintia Gomez <cngomezlab@gmail.com>
2022-12-29 14:02:23 +0100

    Polishing finished. Minor peding details.

--- app.R
+++ app.R
@@ -14,11 +14,6 @@ library(DT)
 library(tidyverse)
 library(ggrepel)
 
-# Pending
-# las modification of name sape
-# orde peraks before saving?
-# The number of peaks behaviour is weird, it is not updated
-
 # Define UI
 ui <- fluidPage(theme = shinytheme("united"),
                 # App name
@@ -148,7 +143,6 @@ server <- function(input, output, session) {
                  label = "Select maximun number of peaks",
                  value = 3,
                  min = 1,
-                 max = 5,
                  step = 1)
   })
   # Select calculated peaks to plot
@@ -164,7 +158,7 @@ server <- function(input, output, session) {
       "peaks.csv"
     },
     content <- function(file) {
-      write.csv(Df$data, file, row.names = FALSE)
+      write.csv(Df$DataPeaks, file, row.names = FALSE)
     }
   )
   
@@ -195,7 +189,7 @@ server <- function(input, output, session) {
       selectInput(inputId = "InChan",
                   label = "Select a channel",
                   choices = names(InitDf()$Files[[1]]),
-                  selected = Df$data$Channel[SampNum])
+                  selected = Df$DataPeaks$Channel[SampNum])
     })
   })
   # Last used smoothing
@@ -205,7 +199,7 @@ server <- function(input, output, session) {
     output$UiSmoothNum <- renderUI({
       numericInput(inputId = "InSmooth",
                    label = "Adjust smoothing",
-                   value = Df$data$Smoothing[SampNum],
+                   value = Df$DataPeaks$Smoothing[SampNum],
                    min = 0.02,
                    max = 1,
                    step = 0.02)
@@ -218,7 +212,7 @@ server <- function(input, output, session) {
     output$UiWindowNum <- renderUI({
       numericInput(inputId = "InWindow",
                    label = "Adjust window",
-                   value = Df$data$Window[SampNum],
+                   value = Df$DataPeaks$Window[SampNum],
                    min = 1,
                    max = 100,
                    step = 1)
@@ -243,7 +237,8 @@ server <- function(input, output, session) {
       for (i in 1:length(ControlsList)){
         Ls[[i]] <- selectInput(inputId = paste0("InCtrlSample", i),
                               label = ControlsList[i],
-                              choices = names(InitDf()$Files))
+                              choices = c("", names(InitDf()$Files)),
+                              selected = "")
       }
       Ls
     })
@@ -258,7 +253,7 @@ server <- function(input, output, session) {
       for (i in 1:length(PloidyList)){
         Ls[[i]] <- numericInput(inputId = paste0("InCtrlPlo", i),
                                label = PloidyList[i],
-                               value = 3,
+                               value = i,
                                min = 1,
                                step = 1)
       }
@@ -305,9 +300,8 @@ server <- function(input, output, session) {
     data.frame(MaxIndex = MaxIndex, Intensity = Intensity)
   }
   
-  # Create reactive values
+  # Peak calculations
   # Get initial files and information
-  # Read FCS files and calculate initial information
   InitDf <- eventReactive(input$InFiles, {
     FilesLs <- list()
     Channels <- NULL
@@ -317,9 +311,9 @@ server <- function(input, output, session) {
     PointLs <- list()
     G1s <- NULL
     G2s <- NULL
-    
     Names <- NULL
     
+    # Read FCS files and calculate initial information
     for(i in 1:length(input$InFiles[, 1])){
       FilesLs[[i]] <- read.FCS(input$InFiles[[i, 'datapath']], emptyValue = FALSE, alter.names = TRUE)
       Name <- sub(" .*", "", input$InFiles$name[i])
@@ -337,179 +331,22 @@ server <- function(input, output, session) {
     # Return a list with the information
     list(Files = FilesLs, Channels = Channels, Smoothings = Smoothings, Windows = Windows, G1s = G1s, G2s = G2s)
   })
-
-  # Create parameter and results data frame to modify initially calculated data
-  # In this case, it had to be created and reactive value in order to be modified via proxy
-  Df <- reactiveValues(data = NULL)
-  
+  # Create reactive values to modify initially calculated data (It had to be created as reactive value to be modified via proxy)
+  Df <- reactiveValues(DataPeaks = NULL)
+  # Copy information form initial data frame
   observeEvent(input$InFiles, {
     req(InitDf())
-    Df$data <- data.frame(Sample = names(InitDf()$Files),
+    Df$DataPeaks <- data.frame(Sample = names(InitDf()$Files),
                           Channel = InitDf()$Channels,
                           Smoothing = InitDf()$Smoothings,
                           Window = InitDf()$Windows,
                           G1 = InitDf()$G1s,
                           G2 = InitDf()$G2s)
     
-    output$ResDf1 <- DT::renderDataTable(isolate(Df$data),
+    output$ResDf1 <- DT::renderDataTable(isolate(Df$DataPeaks),
                                         editable = FALSE)
   })
-  
-  #Df3 <- reactiveValues(data = NULL)
-  # observe more things? correct things?
-  # init Df2 that updates with df?
-  #observeEvent(c(input$InFiles, input$InPeaksPlot, input$InNumCtrl), {
-  #  req(InitDf())
-    #Df$data2 <- Df$data
-    
-  #  Df$data2 <- Df$data[,c(1,5,6)]
-    #Df2$data <- Df$data[,c(1,5,6)]
-    #Df2$data <- Df$data
-    # Combine G1 and G2 fluorescent intensity
-    #Df2$data <- gather(data = Df2$data,
-  #  Df$data2 <- gather(data = Df$data2,
-  #                key = "Phase",
-  #                value = "Intensity",
-  #                -Sample)
-    # Add ploidy
-  #  Df$data2$Ploidy <- NA#input$InNumCtrl
-    
-    # Order alphabetically
-  #  Df$data2 <- Df$data2[order(Df$data2$Sample),]
-  #  output$ResDf2 <- DT::renderDataTable(isolate(Df$data2),
-  #                                       editable = FALSE)
-  #})
-  
-  # Try to select dataframe per sample
-  #observeEvent(input$CtrlPlo, {
-  # What to observe InNumCtrl
-  #MyThingsToObs <- "casa"#paste0("input$InCtrlSample", 1:input$InNumCtrl)
-  
-  #observeEvent(c(input$InFiles, input$InCtrlSample1), {
-  
-  
-  observeEvent(input$InReg, {
-  #observeEvent(eval(parse(text = paste("c(", paste0("input$InCtrlSample", 1:input$InNumCtrl, collapse = ", "), ")"))), {
-    req(input$InCtrlSample1)
-    
-    # Select required information from Df
-    Df$data2 <- Df$data[,c(1,5,6)]
-    
-    # Combine G1 and G2 fluorescent intensity
-    Df$data2 <- gather(data = Df$data2,
-                       key = "Phase",
-                       value = "Intensity",
-                       -Sample)
-    
-    # Order alphabetically
-    Df$data2 <- Df$data2[order(Df$data2$Sample),]
-    # Rename rownames 
-    #rownames(Df$data2) <- 1:(input$InNumCtrl * 2)
-    
-    
-    
-    Pattern <- NULL
-    #Sliders <- grep("slider", names(input), value = TRUE)
-    #for (Slider in Sliders){
-    #  Pattern <- input$Slider
-    #}
-    #paste0("input$slider", 1:input$InNumCtrl)
-    #input$slider1
-    #Pattern <- paste(names(input)[grep("slider", names(input))], sep = "|")
-    #Pattern
-    # Change to sliders name?
-    CtrlSamples <- paste0("input$InCtrlSample", 1:input$InNumCtrl)
-    #eval(parse(text = Sliders))
-    for (Sample in CtrlSamples){
-      Pattern <- c(Pattern, eval(parse(text = Sample)))
-    }
-    
-    Pattern2 <- Pattern
-    
-    Pattern <- paste(Pattern, collapse = "|")
-    
-    # Here I would have to seprate the dataframes first
-    # in orifinal stran patter ins control, kkepp?
-    #Df$Ctrls <- Df$data2[grep(Pattern, Df$data2$Strain),]
-    Df$Ctrls <- Df$data2[grep(Pattern, Df$data2$Sample),]
-    Df$Tests <- Df$data2[-grep(Pattern, Df$data2$Sample),]
-    
-    # Add test and control type information
-    Df$Ctrls$Type <- "Control"
-    # To control in case you select no controls, but same for test?
-    if (nrow(Df$Tests) > 0){
-      Df$Tests$Type <- "Test"
-    }
-    #Df$Tests$Type <- "Test"
-    
-   
-    
-    Ploidies <- paste0("input$InCtrlPlo", 1:input$InNumCtrl)
-    
-    AllPloidies <- NULL
-    
-    # Same but to obtain ploidues
-    for (Ploidy in Ploidies){
-      AllPloidies <- c(AllPloidies, eval(parse(text = Ploidy)))
-    }
-    
-    # Change order of all ploidies to match the one in the data frame
-    
-    Idx <- match(unique(Df$Ctrls$Sample), Pattern2)
-    
-    AllPloidies <- AllPloidies[Idx]
-    
-    AllPloidies <- rep(AllPloidies, each = 2) * 1:2#NA#input$InNumCtrl
-    
-    
-    
-    # Add ploidy
-    Df$Ctrls$Ploidy <- AllPloidies
-    Df$Ctrls$Ploidy2 <- AllPloidies
-    # maybe above
-    Df$Ctrls$Intensity <- as.numeric(Df$Ctrls$Intensity)
-    Df$Tests$Intensity <- as.numeric(Df$Tests$Intensity)
-    
-    
-    
-    # Linear model
-    Mod <- lm(Ploidy ~ Intensity, data = Df$Ctrls)
-    
-    # Summary
-    # render text?
-    #summary(Mod)
-    
-    # Prediction
-    Df$Tests$Ploidy <- predict(Mod, Df$Tests) %>% round(2) # round can be changed
-    Df$Tests$Ploidy2 <- predict(Mod, Df$Tests) %>% round(0) # round can be changed
-    
-    # Combine results
-    Df$Res <- rbind(Df$Ctrls, Df$Tests)
-    
-    # Before rendering data frame, change rownames
-    rownames(Df$Res) <- 1:nrow(Df$Res)
-    
-    # Final output data frame
-    output$ResDf2 <- DT::renderDataTable(isolate(Df$Res),
-                                         editable = FALSE)
-    
-    output$RegPlot <- renderPlot({
-      ggplot(data = Df$Res, mapping = aes(Intensity, Ploidy)) +
-        geom_point(color = "red") +
-        geom_smooth(method = "lm", se = FALSE, linetype = "dashed", color = "black") +
-      #geom_point(data = DfTests, color = "blue") +
-      #geom_text(label = DfRes$Strain)
-      #geom_text_repel(label = DfRes$Strain)
-        geom_text_repel(label = paste(Df$Res$Sample, Df$Res$Phase))
-    })
-    
-    #Df$Ctrls <- Df$data[grep(Pattern, Df$data$Sample),]
-    
-    
-    #output$ResDf3 <- DT::renderDataTable(isolate(Df$Ctrls),
-    #                                     editable = FALSE)
-  })
-  
+
   # Create plot line
   PlotLine <- eventReactive(c(input$InSample, input$InSmooth, input$InWindow, input$InChan), {
     req(InitDf())
@@ -520,7 +357,7 @@ server <- function(input, output, session) {
     # Get channels information
     ChanNum <- grep(input$InChan, names(File))
     # Return Line
-    GetLine(File = File, ChanNum = ChanNum, Span = Df$data[SampNum, 3])
+    GetLine(File = File, ChanNum = ChanNum, Span = Df$DataPeaks[SampNum, 3])
   })
   
   # Create plot points
@@ -529,7 +366,7 @@ server <- function(input, output, session) {
     # Get sample name
     SampNum <- grep(input$InSample, names(InitDf()$Files))
     # Get width
-    Width <- Df$data[SampNum, 4]
+    Width <- Df$DataPeaks[SampNum, 4]
     # Return points
     GetPoints(Width = Width, PlotLine = PlotLine())
   })
@@ -545,11 +382,11 @@ server <- function(input, output, session) {
     
     SampNum <- grep(input$InSample, names(InitDf()$Files))
     # Get name for plotting and table
-    #Name <- names(InitDf()$Files)
     Name <- names(InitDf()$Files)[SampNum]
     # Number of labels
     LabNum <- match(input$InPeaksPlot, PlotPoints()$MaxIndex)
-    Labs <- c("G1", "G2", "G3", "G4", "G5")
+    # Labels
+    Labs <- paste0("G", 1:length(PlotPoints()$MaxIndex))
     # Plot histogram with points
     ggplot(PlotLine(), aes(x = Fluorescence, y = Freq)) +
       geom_line() +
@@ -565,58 +402,134 @@ server <- function(input, output, session) {
   })
 
   # Proxys for data replacement
+  # Channel
   observeEvent(input$InChan, {
     SampNum <- grep(input$InSample, names(InitDf()$Files))
-    # Update smooth
-    Df$data[SampNum, 2] <- input$InChan
+    # Update channel
+    Df$DataPeaks[SampNum, 2] <- input$InChan
     # Replace data
     Proxy <- DT::dataTableProxy('ResDf1')
-    DT::replaceData(Proxy, Df$data)
+    DT::replaceData(Proxy, Df$DataPeaks)
   })
   # Smoothing
   observeEvent(input$InSmooth, {
     SampNum <- grep(input$InSample, names(InitDf()$Files))
     # Update smooth
-    Df$data[SampNum, 3] <- input$InSmooth
+    Df$DataPeaks[SampNum, 3] <- input$InSmooth
     # Replace data
     Proxy <- DT::dataTableProxy('ResDf1')
-    DT::replaceData(Proxy, Df$data)
+    DT::replaceData(Proxy, Df$DataPeaks)
   })
   # Window
   observeEvent(input$InWindow, {
     SampNum <- grep(input$InSample, names(InitDf()$Files))
     # Update window
-    Df$data[SampNum, 4] <- input$InWindow
+    Df$DataPeaks[SampNum, 4] <- input$InWindow
     # Replace data
     Proxy <- DT::dataTableProxy('ResDf1')
-    DT::replaceData(Proxy, Df$data)
+    DT::replaceData(Proxy, Df$DataPeaks)
   })
   # Peaks
   observeEvent(input$InPeaksPlot, {
     SampNum <- grep(input$InSample, names(InitDf()$Files))
     # Update G1 and G2 peaks
-    Df$data[SampNum, 5] <- input$InPeaksPlot[1]
-    Df$data[SampNum, 6] <- input$InPeaksPlot[2]
+    Df$DataPeaks[SampNum, 5] <- input$InPeaksPlot[1]
+    Df$DataPeaks[SampNum, 6] <- input$InPeaksPlot[2]
     # Replace data
     Proxy <- DT::dataTableProxy('ResDf1')
-    DT::replaceData(Proxy, Df$data)
-  })
-  # Controls ploidy
-  # is this working?
-  observeEvent(input$InCtrlPlo1, {
-#    req(Df2)
-#    Df2$Ploidy <- input$InNumCtrl
-    #SampNum <- grep(input$InSample, names(InitDf()$Files))
-    # Update smooth
-    Df$data2$Ploidy <- input$InCtrlPlo1
-    # Replace data
-    Proxy <- DT::dataTableProxy('ResDf2')
-    DT::replaceData(Proxy, Df$data2)
+    DT::replaceData(Proxy, Df$DataPeaks)
   })
   
-  
-  
+  # Regression calculations
+  observeEvent(input$InReg, {
+    req(input$InCtrlSample1)
+    # Select required information from DataPeaks
+    Df$DataReg <- Df$DataPeaks[,c(1,5,6)]
+    # Combine G1 and G2 fluorescent intensity
+    Df$DataReg <- gather(data = Df$DataReg,
+                         key = "Phase",
+                         value = "Intensity",
+                         -Sample)
+    # Convert intensity to numeric
+    Df$DataReg$Intensity <- as.numeric(Df$DataReg$Intensity)
+    # Order alphabetically
+    Df$DataReg <- Df$DataReg[order(Df$DataReg$Sample),]
+    # Initialize variable
+    Pattern <- NULL
+    # Create control sample strings to evaluate
+    CtrlSamples <- paste0("input$InCtrlSample", 1:input$InNumCtrl)
+    # Loop over strings
+    for (Sample in CtrlSamples){
+      # Evaluate each string to obtain the input stored value
+      Pattern <- c(Pattern, eval(parse(text = Sample)))
+    }
+    # Create pattern to serach samples
+    PatternSearch <- paste(Pattern, collapse = "|")
+    # Seprate controls and test data frames
+    Df$Ctrls <- Df$DataReg[grep(PatternSearch, Df$DataReg$Sample),]
+    Df$Tests <- Df$DataReg[-grep(PatternSearch, Df$DataReg$Sample),]
+    # Add test and control type information
+    Df$Ctrls$Type <- "Control"
+    # To control in case no controls are selected
+    if (nrow(Df$Tests) > 0){
+      Df$Tests$Type <- "Test"
+    }
+    # Create control ploidy strings to evaluate
+    CtrlPloidies <- paste0("input$InCtrlPlo", 1:input$InNumCtrl)
+    # Initialize variable
+    AllPloidies <- NULL
+    # Loop over strings
+    for (Ploidy in CtrlPloidies){
+      # Evaluate each string to obtain the input stored value
+      AllPloidies <- c(AllPloidies, eval(parse(text = Ploidy)))
+    }
+    # Change order of all ploidies to match the one in the data frame
+    AllPloidies <- AllPloidies[match(unique(Df$Ctrls$Sample), Pattern)]
+    # Multiply ploidy for G2 peaks
+    AllPloidies <- rep(AllPloidies, each = 2) * 1:2
+    # Add ploidy
+    Df$Ctrls$Ploidy <- AllPloidies
+    # Add rounded ploidy (in this case, it is the same)
+    Df$Ctrls$Rounded <- AllPloidies
+    # Linear model
+    Mod <- lm(Ploidy ~ Intensity, data = Df$Ctrls)
+    
+    # Summary
+    # render text? or plot in graph???
+    #summary(Mod)
+    
+    # Perform prediction
+    Df$Tests$Ploidy <- predict(Mod, Df$Tests) %>% round(2)
+    # Add rounded ploidy
+    Df$Tests$Rounded <- round(Df$Tests$Ploidy, 0)
+    # Combine results
+    Df$Res <- rbind(Df$Ctrls, Df$Tests)
+    
+    # Before rendering data frame, change row names and column order
+    rownames(Df$Res) <- 1:nrow(Df$Res)
+    Df$Res <- Df$Res[,c(1, 4, 2, 3, 5, 6)]
+    # Final output data frame
+    output$ResDf2 <- DT::renderDataTable(isolate(Df$Res),
+                                         editable = FALSE)
+    # Plot regression
+    output$RegPlot <- renderPlot({
+      ggplot(data = Df$Res, mapping = aes(Intensity, Ploidy)) +
+        geom_point(color = "red") +
+        geom_smooth(method = "lm", se = FALSE, linetype = "dashed", color = "black") +
+        geom_text_repel(label = paste(Df$Res$Sample, Df$Res$Phase))
+    })
+  })
 }
 
 # Create Shiny object
 shinyApp(ui = ui, server = server)
+
+
+# Pending
+# las modification of name sape
+# orde peraks before saving?
+# The number of peaks behaviour is weird, it is not updated
+# add summary or R2 in plot? with p?
+# Obtain average of both peaks and give a final result?
+# Help section
+# Dowload final data perhaps only and not the peaks?
