88ab3d03eb979d775a5a87034a37a00a5ed4b6b7
Cintia Gomez <cngomezlab@gmail.com>
2024-01-04 13:32:30 +0100

    Help in markdown format added.

--- app.R
+++ app.R
@@ -14,6 +14,7 @@ library(DT)
 library(tidyverse)
 library(ggrepel)
 library(gridExtra)
+library(markdown)
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("united"),
@@ -125,7 +126,6 @@ ui <- fluidPage(theme = shinytheme("united"),
                            ),
                            # Panel 4
                            tabPanel("Help",
-                                    "In construction...",
                                     uiOutput('textWithHTML')
                            )
                 )
@@ -846,35 +846,9 @@ server <- function(input, output, session) {
   
   # Panel 4 help
   output$textWithHTML <- renderUI({
-    rawText <- readLines('help.txt')
-    splitText <- stringi::stri_split(str = rawText, regex = '\\n')
-    replacedText <- lapply(splitText, p)
-    
-    return(replacedText)
+    HTML(markdown::markdownToHTML('help.md', template = FALSE))
   })
 }
 
 # Create Shiny object
 shinyApp(ui = ui, server = server)
-
-
-# Pending
-# Help section
-# Explain in help what is the plotted r and the pvalue
-
-# Known issues
-# App works deletes ".fcs" and ".FCS" extensions from files names, and removes everything after a space to generate the sample name
-# If after this previous process duplicated names are generated, the app will crash, thus it is better to name the files with unique names
-# Sometimes it crashes when loading different samples, so it is better to close and re-run the app
-# Notice that when modifying the smoothing and window, it recalculates and deselect already chosen peaks
-# When one peak value is empty it does not do plot the Rsquared and Pvalue
-# When files are large and exceed the maximum size, prior running the app you can set in your console: options(shiny.maxRequestSize = 10 * 1024^2)
-# It will give an error in window (span) is too small
-
-# Potential improvements
-# Add safe code to avoid downloading empty documents
-# Add code to download individual histograms and linear regression plots
-# Add code to download regression summary as text
-# Add code to exclude samples from final summary histograms
-# Add code to download all detected peaks when more than two are detected
-# Generate new version of script to resume analysis using previously determined smoothing and window
---
+++ help.md
@@ -0,0 +1,35 @@
+## Help
+
+Sorry, we are in construction...
+
+### Pre-requisites
+
+These are the libraries required to run MuPET-FC. Here we provide links to their source or installation instructions.
+
+* [shiny](https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/index.html)
+* [shinythemes](https://rstudio.github.io/shinythemes/)
+* [flowCore](https://bioconductor.org/packages/release/bioc/html/flowCore.html)
+* [zoo](https://cran.r-project.org/web/packages/zoo/index.html)
+* [ggplot2](https://ggplot2.tidyverse.org/)
+* [DT](https://rstudio.github.io/DT/)
+* [tidyverse](https://www.tidyverse.org/packages/)
+* [ggrepel](https://cran.r-project.org/web/packages/ggrepel/readme/README.html)
+* [gridExtra](https://cran.r-project.org/web/packages/gridExtra/index.html)
+* [markdown](https://cran.r-project.org/web/packages/markdown/index.html)
+
+### Known issues
+
+* App works deletes `.fcs` and `.FCS` extensions from files names, and removes everything after a space to generate the sample name. If after this previous process duplicated names are generated, the app will crash, thus it is better to name the files with unique names
+* Sometimes it crashes when uploading different samples, so it is better to close and re-run the app.
+* Notice that when modifying the smoothing and window, it recalculates and deselect already chosen peaks.
+* When files are large and exceed the maximum size, prior running the app you can set in your console: `options(shiny.maxRequestSize = 10 * 1024^2)`.
+* It will give an error if window (span) is too small.
+
+### Potential improvements
+* Add safe code to avoid downloading empty documents.
+* Add code to download individual histograms and linear regression plots.
+* Add code to download regression summary as text.
+* Add code to exclude samples from final summary histograms.
+* Add code to download all detected peaks when more than two are detected.
+* Generate new version of script to resume analysis using previously determined smoothing and window.
+* Explain in help what is the plotted r and the pvalue.
